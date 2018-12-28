55931625bbe10c88d7faafba6ded13a8750ecd7e
Catalin Georgescu <catalin.ioan.georgescu@gmail.com>
2018-12-28 12:28:58 +0200

    Moving common-springboot-springdata into the FX-UTILS project as fx-utils-springboot-data

--- server/game-world-registry-springdata/src/main/java/com/crazydumplings/backend/gameworldregistry/springdata/config/GameWorldRegistryDatasourceConfig.java
+++ server/game-world-registry-springdata/src/main/java/com/crazydumplings/backend/gameworldregistry/springdata/config/GameWorldRegistryDatasourceConfig.java
@@ -18,7 +18,7 @@ import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-import com.crazydumplings.common.springboot.data.DataSourceConfigUtils;
+import com.desolatetimelines.utils.springboot.data.DataSourceConfigUtils;
 
 
 @Configuration
