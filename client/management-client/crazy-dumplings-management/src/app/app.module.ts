c7c528ad664c8667fb9d5e8fe1fcfbda71f3884b
Catalin Georgescu <catalin.ioan.georgescu@gmail.com>
2019-01-07 17:28:25 +0200

    Testing out PrimeFaces

--- client/management-client/crazy-dumplings-management/src/app/app.module.ts
+++ client/management-client/crazy-dumplings-management/src/app/app.module.ts
@@ -3,12 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { MainComponent } from './components/main/main.component';
 
+// PrimeFaces
+import {ToolbarModule} from 'primeng/toolbar';
+import {ButtonModule} from 'primeng/button';
+
 @NgModule({
   declarations: [
     MainComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ToolbarModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [MainComponent]
