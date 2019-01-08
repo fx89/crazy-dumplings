a196ef4c8ce40bcabcce4b14d14e7dfd595719b2
Catalin Georgescu <catalin.ioan.georgescu@gmail.com>
2019-01-08 10:37:55 +0200

    Standardizing package names and class locations and stuff

--- server/management-endpoint-common/management-endpoint-common-web/src/main/java/com/crazydumplings/backend/common/web/rest/CrazyDumplingsHostileHttpResponseCodesController.java
+++ server/management-endpoint-common/management-endpoint-common-web/src/main/java/com/crazydumplings/backend/common/web/rest/CrazyDumplingsHostileHttpResponseCodesController.java
@@ -1,4 +1,4 @@
-package com.crazydumplings.backend.common.web;
+package com.crazydumplings.backend.common.web.rest;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
