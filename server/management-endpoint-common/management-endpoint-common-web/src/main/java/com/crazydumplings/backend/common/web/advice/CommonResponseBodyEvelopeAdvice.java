77d991d736d283c1610b5bdd98b790736efd9f61
Catalin Georgescu <catalin.ioan.georgescu@gmail.com>
2019-01-10 11:29:45 +0200

    With the newest version of Spring Boot and JDK 11, exception handling seems to be working

--- server/management-endpoint-common/management-endpoint-common-web/src/main/java/com/crazydumplings/backend/common/web/advice/CommonResponseBodyEvelopeAdvice.java
+++ server/management-endpoint-common/management-endpoint-common-web/src/main/java/com/crazydumplings/backend/common/web/advice/CommonResponseBodyEvelopeAdvice.java
@@ -2,18 +2,23 @@ package com.crazydumplings.backend.common.web.advice;
 
 import static com.crazydumplings.backend.common.web.GenericMessages.OPERATION_SUCCESSFUL;
 import static com.desolatetimelines.utils.lang.StringUtils.nvl;
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 import static org.springframework.http.HttpStatus.OK;
 import static org.springframework.http.HttpStatus.UNAUTHORIZED;
 
 import java.io.Serializable;
-import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.core.MethodParameter;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
 
@@ -25,31 +30,22 @@ public class CommonResponseBodyEvelopeAdvice implements ResponseBodyAdvice<Objec
         return true;
     }
 
-    @SuppressWarnings("unchecked")
-	@Override
+    @Override
     public Object beforeBodyWrite(Object body, MethodParameter returnType, MediaType selectedContentType, Class<? extends HttpMessageConverter<?>> selectedConverterType, ServerHttpRequest request, ServerHttpResponse response) {
-     // Compute the HTTP status:
-     // If there's an error, then compute the http status based on the error message, otherwise assume the status is OK
-        HttpStatus httpStatus = returnType.getExecutable().getName().equals("error") ? mapExceptionToHttpStatus((Map<String, Object>) body) : OK;
-
-     // Overwrite the response codes
-        response.setStatusCode(httpStatus);
-
-     // Return the payload
-        return new ApiResponse(httpStatus, (Serializable) nvl(body, OPERATION_SUCCESSFUL));
+        return new ApiResponse(OK, (Serializable) nvl(body, OPERATION_SUCCESSFUL));
     }
 
- // TODO: find out why exception handling doesn't produce any effect even though it's used like in the tutorial: http://www.springboottutorial.com/spring-boot-exception-handling-for-rest-services
-    private static HttpStatus mapExceptionToHttpStatus(Map<String, Object> body) {
-        String message = (String) body.get("message");
+    @ExceptionHandler(org.springframework.security.access.AccessDeniedException.class)
+    @ResponseBody
+    @ResponseStatus(UNAUTHORIZED) // this can also be set to the exception itself
+    public ApiResponse handleAccessDeniedExceptions(Exception ex, HttpServletRequest request, HttpServletResponse response) {
+        return new ApiResponse(UNAUTHORIZED, ex.getMessage());
+    }
 
-        if (message != null) {
-            message = message.toUpperCase();
-
-            if (message.contains("ACCESS") && message.contains("DENIED"))
-                return UNAUTHORIZED;
-        }
-
-        return HttpStatus.valueOf((Integer) body.get("status"));
+    @ExceptionHandler(Exception.class)
+    @ResponseBody
+    @ResponseStatus(INTERNAL_SERVER_ERROR)
+    public ApiResponse handleOtherExceptions(Exception ex, HttpServletRequest request, HttpServletResponse response) {
+        return new ApiResponse(INTERNAL_SERVER_ERROR, (Serializable)nvl(ex.getMessage(), ex.getClass().getName()));
     }
 }
