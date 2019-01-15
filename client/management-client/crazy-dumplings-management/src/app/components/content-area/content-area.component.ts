fb6778bd0953461d480bfa2c51ffb7bdca8ff808
Catalin Georgescu <catalin.ioan.georgescu@gmail.com>
2019-01-15 14:37:09 +0200

    Adding slide in / slide out transition

--- client/management-client/crazy-dumplings-management/src/app/components/content-area/content-area.component.ts
+++ client/management-client/crazy-dumplings-management/src/app/components/content-area/content-area.component.ts
@@ -2,12 +2,40 @@ import { Component, OnInit } from '@angular/core';
 import { StatefulViewVariablesService } from '../../services/stateful-view-variables/stateful-view-variables.service';
 import { AppSection } from '../../services/stateful-view-variables/stateful-view-variables.service';
 
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+} from '@angular/animations';
+
 
 
 @Component({
   selector: 'app-content-area',
   templateUrl: './content-area.component.html',
-  styleUrls: ['./content-area.component.less']
+  styleUrls: ['./content-area.component.less'],
+
+  animations: [
+    trigger('slideInAndOut', [
+      // ...
+      state('slideIn', style({
+        opacity: '1',
+        transform: 'translateX(0%)'
+      })),
+      state('slideOut', style({
+        opacity: '0',
+        transform: 'translateX(-100%)'
+      })),
+      transition('slideIn => slideOut', [
+        animate('1s')
+      ]),
+      transition('slideOut => slideIn', [
+        animate('0.5s')
+      ]),
+    ]),
+  ]
 })
 export class ContentAreaComponent implements OnInit {
 
