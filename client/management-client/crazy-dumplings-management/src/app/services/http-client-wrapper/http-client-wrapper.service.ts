32bd1b7b7237f8fdc0adccf3ad1f422a8c330ade
Catalin Georgescu <catalin.ioan.georgescu@gmail.com>
2019-01-16 22:00:59 +0200

    Removing imports which are not used

--- client/management-client/crazy-dumplings-management/src/app/services/http-client-wrapper/http-client-wrapper.service.ts
+++ client/management-client/crazy-dumplings-management/src/app/services/http-client-wrapper/http-client-wrapper.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders, HttpParams, HttpRequest, HttpEvent } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 
